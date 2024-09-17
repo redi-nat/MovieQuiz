494ed165786d109235bb08e97971c98df518d70d
Natasha <nredinskaya@gmail.com>
2024-09-17 14:58:55 +0200

    Внесла правки 2.0

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -24,7 +24,7 @@ final class MovieQuizUITests: XCTestCase {
     }
     
     func testYesButton() {
-        sleep(20)
+        sleep(5)
         
         let firstPoster = app.images["Poster"]
         let firstPosterData = firstPoster.screenshot().pngRepresentation
@@ -38,8 +38,7 @@ final class MovieQuizUITests: XCTestCase {
         let indexLabel = app.staticTexts["Index"]
        
         XCTAssertNotEqual(firstPosterData, secondPosterData)
-        XCTAssertEqual(indexLabel.label, "1/10")
-        print(indexLabel)
+        XCTAssertEqual(indexLabel.label, "2/10")
     }
     
     func testNoButton() {
